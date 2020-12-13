fbefbb00afb62ef3609a799a17194062957f44b2
Arjit-Shivansh <shivhack.earth@gmail.com>
2020-12-13 11:51:13 +0530

    sixth_commit

--- src/contexts/SocketProvider.js
+++ src/contexts/SocketProvider.js
@@ -11,7 +11,7 @@ export function SocketProvider({ id, children }) {
   const [socket, setSocket] = useState();
 
   useEffect(() => {
-    const newSocket = io("http://localhost:5000", {
+    const newSocket = io("https://chit-chaat-bknd.herokuapp.com", {
       query: { id },
       transports: ["polling", "websocket"],
     });
